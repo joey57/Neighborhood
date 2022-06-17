9fb0347018413fff70a8ddded1d32667f9909622
joey57 <njorogejoey5@gmail.com>
2022-06-17 16:49:09 +0300

    create update profile url

--- hood/urls.py
+++ hood/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-  path('', views.index, name = 'index')
+  path('', views.index, name = 'index'),
+  path('update_profile/<username>/edit/', views.update_profile, name='update-profile'),
 
 ]
 if settings.DEBUG:
