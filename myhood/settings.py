3b44ffc8a18e4c4803840b06b8e4c19fc081a815
joey57 <njorogejoey5@gmail.com>
2022-06-17 15:05:01 +0300

    install crispy forms

--- myhood/settings.py
+++ myhood/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'hood',
     'django_bootstrap5',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -129,6 +130,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
