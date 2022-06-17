c3a6b42decaf9b10a0ad4a66d2578ca8ccbb398c
joey57 <njorogejoey5@gmail.com>
2022-06-17 16:49:38 +0300

    add update profile view function

--- hood/views.py
+++ hood/views.py
@@ -1,9 +1,11 @@
+from cmath import log
 from django.shortcuts import render, redirect
 from django.http  import HttpResponse, Http404, HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UpdateProfileForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
@@ -22,5 +24,22 @@ def register(request):
   return render(request, 'users/register.html',{"form":form})
 
 @login_required
-def profile(request):
+def profile(request, username):
   return render(request, 'users/profile.html')
+
+@login_required
+def update_profile(request,username):
+  user=User.objects.get(username=username)
+  current_user = request.user
+  
+  if request.method =='POST':
+    form = UpdateProfileForm(request.POST,request.FILES, instance=current_user.profile)
+    
+    if form.is_valid():
+      form.save()
+      return redirect('profile', user.username)
+  
+  else:
+    form = UpdateProfileForm(instance=current_user.profile)
+  return render(request,"users/update_profile.html", {"form":form})
+  
