5f38f05e9ddd00aa35b9515fa43a1166e616bcb4
joey57 <njorogejoey5@gmail.com>
2022-06-17 15:17:48 +0300

    add login and logout url redirect

--- myhood/settings.py
+++ myhood/settings.py
@@ -131,6 +131,11 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = ''
+LOGIN_URL = 'login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
