5537507bf184a29014bad21020152f5da7ebce54
joey57 <njorogejoey5@gmail.com>
2022-06-17 15:04:45 +0300

    create url registration

--- myhood/urls.py
+++ myhood/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from hood import views as hood_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hood.urls')),
+    path('register/', hood_views.register, name = 'register'),
 ]
